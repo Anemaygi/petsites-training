4db223d4d731540d1067955115fcb109268c67b8
Anemaygi <gianemayumisan@gmail.com>
2024-05-03 16:40:52 +0800

    explicando states

--- src/app/membros/page.tsx
+++ src/app/membros/page.tsx
@@ -1,12 +1,19 @@
+'use client'
 import MyComponent from "@/components/myComponent";
 import Name from "./name";
 import Data from "./data.json";
+import { useState } from "react";
+
+
 
 export default function Membros() {
-  return (
-    <div>
-        <MyComponent nomes={Data.names} />
-        <Name />
-    </div>
+    const [nome,setNome] = useState('a');
+    return (
+        <div>
+            <MyComponent nomes={Data.names} />
+            {nome}
+            <div onClick={() => setNome("aaaaa")} className="bg-green-500" >Clica aquiiii</div>
+            <Name />
+        </div>
   );
 }
