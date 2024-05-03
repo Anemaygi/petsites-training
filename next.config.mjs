33d144afeee0cb1419650be2d4a0463782c11b9e
Anemaygi <gianemayumisan@gmail.com>
2024-05-03 13:28:04 +0800

    chore

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/teste-ghactions",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
